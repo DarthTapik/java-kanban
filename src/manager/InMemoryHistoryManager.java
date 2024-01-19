2db3e247538a475b4daedb39c171e65d01d2bb53
JustTapik <oleg.studyperegud@gmail.com>
2024-01-19 22:11:49 +0300

    Created HistoryManager interface, created InMemoryHistoryManager class

---
+++ src/manager/InMemoryHistoryManager.java
@@ -0,0 +1,22 @@
+package manager;
+
+import tasks.Task;
+
+import java.util.ArrayList;
+
+public class InMemoryHistoryManager implements HistoryManager{
+
+    static ArrayList<Task> history = new ArrayList<>();
+
+    @Override
+    public ArrayList<Task> getHistory() {
+        return history;
+    }
+    @Override
+    public void add(Task task){
+        if (history.size() == 10){
+            history.remove(0);
+        }
+        history.add(task);
+    }
+}
