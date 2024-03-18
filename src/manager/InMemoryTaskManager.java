eb11c17b50c9842f8f9cde2444f1e9616f2d9719
DarthTapik <darthtapik@yandex.ru>
2024-03-18 13:43:52 +0300

    Add field sortedList and method getPrioritizedTasks, create comparator TaskByStartTimeComparator

--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -3,13 +3,17 @@ package manager;
 import tasks.Epic;
 import tasks.SubTask;
 import tasks.Task;
+import tasks.TaskByStartTimeComparator;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class InMemoryTaskManager implements TaskManager {
     private Map<Integer, Task> taskList = new HashMap<>();
     private Map<Integer, SubTask> subTaskList = new HashMap<>();
     private Map<Integer, Epic> epicList = new HashMap<>();
+    private Comparator<Task> taskComparator = new TaskByStartTimeComparator();
+    private Set <Task> sortedList = new TreeSet<>(taskComparator);
     protected HistoryManager historyManager = Managers.getDefaultHistory();
     private int id = 0;
     public InMemoryTaskManager(){
@@ -20,9 +24,7 @@ public class InMemoryTaskManager implements TaskManager {
         System.out.println("Задача " + id + " Добавлена");
         task.setId(id);
         taskList.put(id++, new Task(task));
-        /*
-         Не совсем понимаю зачем тут преинкремент, я ввожу в таблицу id после инкрементирую для следующих записей
-        */
+        sortedList.add(new Task(task));
     }
 
     public void addSubTask(SubTask subTask){
@@ -33,7 +35,8 @@ public class InMemoryTaskManager implements TaskManager {
             SubTask subTaskCopy = new SubTask(subTask);
             subTaskList.put(id++, subTaskCopy);
             epic.addSubTask(subTaskCopy);
-            epic.updateStatus();
+            epic.updateStatusAndTime();
+            sortedList.add(new SubTask(subTask));
             return;
         }
         System.out.println("Отсутствует Эпик заданный в подзадаче");
@@ -43,6 +46,9 @@ public class InMemoryTaskManager implements TaskManager {
         System.out.println("Эпик " + id + " Добавлен");
         epic.setId(id);
         epicList.put(id++, new Epic(epic));
+        if (epic.getStartTime().isAfter(LocalDateTime.of(1970,1,1,0,0))){
+            sortedList.add(new Epic(epic));
+        }
     }
 
     public List<Task> getAllTask(){
@@ -101,7 +107,10 @@ public class InMemoryTaskManager implements TaskManager {
 
     public void updateTask(Task task){
         if (taskList.containsKey(task.getId())){
+            sortedList.remove(taskList.get(task.getId()));
             taskList.put(task.getId(), task);
+            sortedList.add(task);
+
             System.out.println("Задача " + task.getId() + " Обновлена");
         } else {
             System.out.println("Ключ не найден");
@@ -113,9 +122,11 @@ public class InMemoryTaskManager implements TaskManager {
             if (epicList.containsKey(subTask.getEpicId())) {
                 Epic epic = epicList.get(subTask.getEpicId());
                 epic.removeSubTask(getSubTask(subTask.getId())); // Удаление для избежания повторений в списке
+                sortedList.remove(getSubTask(subTask.getId()));
                 subTaskList.put(subTask.getId(), subTask);
                 epic.addSubTask(subTask);
-                epic.updateStatus();
+                epic.updateStatusAndTime();
+                sortedList.add(subTask);
                 System.out.println("Подзадача " + subTask.getId() + " Обновлена");
             } else {
                 System.out.println("Отстутствует эпик заданный в подзадаче");
@@ -128,6 +139,10 @@ public class InMemoryTaskManager implements TaskManager {
     public void updateEpic(Epic epic){
         if (epicList.containsKey(epic.getId())) {
             epicList.put(epic.getId(), epic);
+            sortedList.remove(getEpic(epic.getId()));
+            if (epic.getStartTime().isAfter(LocalDateTime.of(1970,1,1,0,0))){
+                sortedList.add(new Epic(epic));
+            }
         } else {
             System.out.println("Ключ не найден");
         }
@@ -135,8 +150,10 @@ public class InMemoryTaskManager implements TaskManager {
 
     public void deleteTask(int id){
         if (taskList.containsKey(id)){
+            sortedList.remove(taskList.get(id));
             taskList.remove(id);
             historyManager.remove(id);
+
             System.out.println("Задача " + id + " удалена");
         } else {
             System.out.println("Ключ " + id + " не найден");
@@ -146,11 +163,12 @@ public class InMemoryTaskManager implements TaskManager {
     public void deleteSubTask(int id){
         if (subTaskList.containsKey(id)){
             SubTask subTask = subTaskList.get(id);
+            sortedList.remove(subTask);
             subTaskList.remove(id);
             historyManager.remove(id);
             Epic epic = epicList.get(subTask.getEpicId());
             epic.removeSubTask(subTask);
-            epic.updateStatus();
+            epic.updateStatusAndTime();
             System.out.println("Подзадача " + id + " удалена");
         } else {
             System.out.println("Ключ " + id + " не найден");
@@ -160,8 +178,10 @@ public class InMemoryTaskManager implements TaskManager {
     public void deleteEpic(int id) {
         if (epicList.containsKey(id)) {
             Epic epic = epicList.get(id);
+            sortedList.remove(epic);
             ArrayList<SubTask> subTasks = new ArrayList<>(epic.getSubTaskList()); // клонирование списка
             for (SubTask subTask : subTasks) {
+                sortedList.remove(subTask);
                 historyManager.remove(subTask.getId());
                 deleteSubTask(subTask.getId());
 
@@ -179,6 +199,7 @@ public class InMemoryTaskManager implements TaskManager {
         epicList.clear();
         taskList.clear();
         subTaskList.clear();
+        sortedList.clear();
         historyManager = Managers.getDefaultHistory();
         System.out.println("Все задачи удалены");
     }
@@ -186,7 +207,7 @@ public class InMemoryTaskManager implements TaskManager {
     public void deleteAllTasks(){
         ArrayList<Integer> taskIdList = new ArrayList<>(taskList.keySet());
         for (Integer taskId : taskIdList){
-            deleteSubTask(taskId);
+            deleteTask(taskId);
         }
         System.out.println("Задачи удалены");
     }
@@ -211,8 +232,14 @@ public class InMemoryTaskManager implements TaskManager {
         return historyManager.getHistory();
     }
 
+    public List<Task> getPrioritizedTasks(){
+        return new ArrayList<>(sortedList);
+    }
+
+
     protected void forceAddTask(Task task){ // Метод для добавления задачи, без присвоения id самим менеджером
         taskList.put(task.getId(), task);
+        sortedList.add(task);
         if (task.getId() > id) { // Сравниваю id для того, чтобы избежать повторов и конфликтов при добавлении
             id = task.getId() + 1; // из файла, а после через менеджер
         }
@@ -220,6 +247,9 @@ public class InMemoryTaskManager implements TaskManager {
 
     protected void forceAddEpic(Epic epic){
         epicList.put(epic.getId(), epic);
+        if (epic.getStartTime().isAfter(LocalDateTime.of(1970,1,1,0,0))){
+            sortedList.add(new Epic(epic));
+        }
         if (epic.getId() > id) {
             id = epic.getId() + 1;
         }
@@ -230,6 +260,7 @@ public class InMemoryTaskManager implements TaskManager {
             Epic epic = epicList.get(subTask.getEpicId());
             subTaskList.put(subTask.getId(), subTask);
             epic.addSubTask(subTask);
+            sortedList.add(subTask);
             if (subTask.getId() > id) {
                 id = subTask.getId() + 1;
             }
